edc35f24f1970d9ac154497d761f7ea4c3ca2fd1
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-09-13 21:41:12 +0530

    "Updated getTasksByCategory function to use case-insensitive regex search for category"

--- src/controllers/getTasksByCategory.js
+++ src/controllers/getTasksByCategory.js
@@ -2,7 +2,13 @@ const Task_NodeJs_To_Do_List = require('../models/task');
 
 const getTasksByCategory = async (req, res) => {
     try {
-        const tasks = await Task_NodeJs_To_Do_List.find({ category: req.params.category });
+        const category = req.params.category;
+
+        // Use case-insensitive regex for the category search
+        const tasks = await Task_NodeJs_To_Do_List.find({
+            category: { $regex: new RegExp(`^${category}$`, 'i') }
+        });
+
         res.json(tasks);
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while fetching tasks by category' });
